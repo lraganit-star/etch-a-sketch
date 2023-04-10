3f49f690b3b2c1525882a1a54efccbf3c64f5fb3
Leslie Raganit <niki@Leslies-MacBook-Pro.local>
2023-04-10 13:07:18 -0700

    adding squares to DOM

---
+++ squares.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 16; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('style', 
+                    'width: 50px; height: 50px; border: 2px solid black');
+    document.body.appendChild(div);
+}
