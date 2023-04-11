b7fc49fe21d403bf393b410797827c5b0a2d2354
Leslie Raganit <niki@Leslies-MacBook-Pro.local>
2023-04-11 15:53:32 -0700

    change square color when hovering

--- squares.js
+++ squares.js
@@ -1,6 +1,11 @@
+console.clear()
+
 for (let i = 0; i < 16; i++) {
     const div = document.createElement('div');
     div.setAttribute('style', 
                     'width: 50px; height: 50px; border: 1px solid black');
+    div.classList.add("square");
+    div.addEventListener("mouseover", (event) => event.target.style.backgroundColor = "orange", false);
+    div.addEventListener("mouseout", (event) => event.target.style.backgroundColor = "purple", false);    
     document.getElementById("squares").appendChild(div);
 }
