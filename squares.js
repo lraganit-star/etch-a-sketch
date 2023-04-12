17e570c4ac70b9e50f0e5d6dcd715243c349b265
Leslie Raganit <niki@Leslies-MacBook-Pro.local>
2023-04-12 02:07:37 -0700

    finished project

--- squares.js
+++ squares.js
@@ -1,11 +1,31 @@
-console.clear()
 
-for (let i = 0; i < 16; i++) {
+var popup = document.getElementById("popup");
+
+function popupFunc() {
+    var size = prompt('What do you want the grid size to be?');
+    return size;
+}
+
+var size = popupFunc();
+console.log(size);
+
+for (let i = 0; i < size ** 2; i++) {
     const div = document.createElement('div');
     div.setAttribute('style', 
                     'width: 50px; height: 50px; border: 1px solid black');
     div.classList.add("square");
-    div.addEventListener("mouseover", (event) => event.target.style.backgroundColor = "orange", false);
-    div.addEventListener("mouseout", (event) => event.target.style.backgroundColor = "purple", false);    
+    div.addEventListener("mouseover", (event) => event.target.style.backgroundColor = "orange");
+    div.addEventListener("mouseout", (event) => event.target.style.backgroundColor = "purple");    
     document.getElementById("squares").appendChild(div);
 }
+
+const squares = document.getElementById("squares");
+var width = 52 * size
+squares.style.width = width + 'px'
+
+// clearButton.addEventListener("onclick", onClick)
+
+// function onClick() {
+//     squares.style.backgroundColor = "white";
+// }
+
